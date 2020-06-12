d03e38e2221b96b6bf07e700a21ad4c5422610b5
Magnusson95 <alexander.magnusson95@gmail.com>
2020-06-12 13:34:10 +0000

    Finalised project

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def insert_category():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
